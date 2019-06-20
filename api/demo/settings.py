6f10a4a2373646e557f8cececcbcf06ef2d8dae2
William Galleti <william.galleti@gmail.com>
2019-06-20 08:00:23 -0400

    Remove duplicate SessionAuthentication

--- api/demo/settings.py
+++ api/demo/settings.py
@@ -51,7 +51,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ),
     'COERCE_DECIMAL_TO_STRING': False,
 }
